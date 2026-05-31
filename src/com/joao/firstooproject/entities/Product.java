7eab1a102faa14d772700a0d5a3c1a4b1b599626
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-05-30 22:17:39 -0300

    initial commit

---
+++ src/com/joao/firstooproject/entities/Product.java
@@ -0,0 +1,33 @@
+package com.joao.firstooproject.entities;
+
+public abstract class Product {
+    private String code;
+    private double price;
+    private int quantity;
+
+    public Product() {}
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
