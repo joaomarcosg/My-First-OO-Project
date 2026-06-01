ba7a4d9504e3039ee8d95b9ccdb6b0d512cb2c96
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-06-01 06:57:40 -0300

    emulates database

---
+++ src/com/joao/firstooproject/database/Database.java
@@ -0,0 +1,59 @@
+package com.joao.firstooproject.database;
+
+import com.joao.firstooproject.entities.Client;
+import com.joao.firstooproject.entities.Coupon;
+import com.joao.firstooproject.entities.Order;
+import com.joao.firstooproject.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    private List<Product> products;
+    private List<Order> orders;
+    private List<Coupon> coupons;
+    private Client client;
+
+    public Database() {
+        this.products = new ArrayList<>();
+        this.orders = new ArrayList<>();
+        this.client = new Client();
+
+        this.coupons = new ArrayList<>();
+        coupons.add(new Coupon("COUPON2", 2));
+        coupons.add(new Coupon("COUPON5", 5));
+        coupons.add(new Coupon("COUPON7", 7));
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public Coupon[] getCoupons() {
+        return coupons.toArray(new Coupon[coupons.size()]);
+    }
+
+    public Order[] getOrders() {
+        return orders.toArray(new Order[orders.size()]);
+    }
+
+    public Product[] getProducts() {
+        return products.toArray(new Product[products.size()]);
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(int position) {
+        products.remove(position);
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public void removeOrder(int position) {
+        orders.remove(position);
+    }
+}
