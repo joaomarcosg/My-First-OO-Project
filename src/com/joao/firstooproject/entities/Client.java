31c8d2696a0e42fb3db202507784e9cf218fc9db
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-07-04 20:28:25 -0300

    adds equals and hashCode method to the class

--- src/com/joao/firstooproject/entities/Client.java
+++ src/com/joao/firstooproject/entities/Client.java
@@ -1,5 +1,7 @@
 package com.joao.firstooproject.entities;
 
+import java.util.Objects;
+
 public class Client {
     private String name;
     private String cpf;
@@ -23,6 +25,18 @@ public class Client {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(name, client.name) && Objects.equals(cpf, client.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, cpf);
     }
 
     @Override
