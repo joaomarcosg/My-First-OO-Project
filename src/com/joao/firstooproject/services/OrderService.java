9b19279f7e8e6eb9dcb7cf149e2a0c25bd1e270e
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-07-06 22:03:06 -0300

    feat: adds methods save, getAll and calculateShipping to the service class

--- src/com/joao/firstooproject/services/OrderService.java
+++ src/com/joao/firstooproject/services/OrderService.java
@@ -5,6 +5,7 @@ import com.joao.firstooproject.entities.Coupon;
 import com.joao.firstooproject.entities.Order;
 import com.joao.firstooproject.entities.Product;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class OrderService {
@@ -12,6 +13,45 @@ public class OrderService {
 
     public OrderService(Database database) {
         this.database = database;
+    }
+
+    private double calculateTotal(List<Product> products, Coupon coupon) {
+        double total = 0.0;
+        for (Product product : products) {
+            total += product.calculateShipping;
+        }
+
+        if (coupon != null) {
+            return total * (1 - coupon.getDiscount());
+        } else {
+            return total;
+        }
+    }
+
+    public void save(Order newOrder) {
+        save(newOrder, null);
+    }
+
+    public void save(Order newOrder, Coupon coupon) {
+        String code = "PE%4d%2d%004d";
+        LocalDate today = LocalDate.now();
+        code = String.format(code, today.getYear(), today.getMonthValue(), database.getOrders().length);
+
+        newOrder.setCode(code);
+        newOrder.setClient(database.getClient());
+        newOrder.setTotal(calculateTotal(newOrder.getProducts(), coupon));
+        database.addOrder(newOrder);
+        System.out.println("Order saved successfully");
+    }
+
+    public void getAll() {
+        if (database.getOrders().length == 0) {
+            System.out.println("There are no orders");
+        } else {
+            for (Order order : database.getOrders()) {
+                System.out.println(order.toString());
+            }
+        }
     }
 
     public void delete(String code) {
